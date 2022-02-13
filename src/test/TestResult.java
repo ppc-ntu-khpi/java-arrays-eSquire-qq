27e23660737e009b19faef1d8195254c11e1b07a
Artem Teslia <91652654+eSquire-qq@users.noreply.github.com>
2022-02-13 16:13:12 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,7 +5,11 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-        int number = 371, originalNumber = 371, remainder = 0, result = 0;
+        System.out.println("Enter your number: ");
+        
+        Scanner in1 = new Scanner(System.in);
+
+        int number = in1.nextInt(), originalNumber = number, remainder = 0, result = 0;
 
         Calculate(number,originalNumber,remainder, result);
     }
